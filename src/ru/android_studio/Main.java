edc46a621e54ba4188d96c87356c60034ed6324d
Yury Andreev <Yury.Andreev@ru.ibm.com>
2016-11-23 11:59:58 +0300

    Initial commit

---
+++ src/ru/android_studio/Main.java
@@ -0,0 +1,33 @@
+package ru.android_studio;
+
+import net.lightbody.bmp.BrowserMobProxy;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+
+public class Main {
+
+    private static final String WEB_DRIVER_PATH = "/Users/yuryandreev/IBM/untitled1/";
+    private static final String CHROMEDRIVER = "chromedriver";
+    private static BrowserMobProxy proxy; // for use proxy, use Props proxy.enable = true
+
+    public static void main(String[] args) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        File file = new File(WEB_DRIVER_PATH + CHROMEDRIVER);
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("chrome.switches", "--disable-extensions");
+        capabilities.setBrowserName("chrome");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        ChromeDriver chromeDriver = new ChromeDriver(capabilities);
+        chromeDriver.get("http://test.com");
+    }
+}
